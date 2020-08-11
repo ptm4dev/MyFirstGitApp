63082a01e0664ec9d675597025b18e167d85cb27
Prince Thomas <developer.princet8@gmail.com>
2020-08-11 16:21:02 +0530

    display version name

--- app/src/main/java/com/example/myfirstgitapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstgitapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.myfirstgitapp
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv.text = getAppVersion(this)
+
+    }
+
+    fun getAppVersion(context: Context): String {
+        var version = ""
+        try {
+            val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+            version = pInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+
+        return version
+
     }
 }
